ed8bef88821739451bdbc2e0a4b86433af323476
Oliver Hookins <ohookins@gmail.com>
2013-01-13 09:49:30 +0100

    Add day one of Prolog exercises.

---
+++ prolog/books.pl
@@ -0,0 +1,9 @@
+book_author('ulysses', 'james joyce').
+book_author('lolita', 'vladimir nabokov').
+book_author('brave new world', 'aldous huxley').
+book_author('catch-22', 'joseph heller').
+book_author('point counter point', 'aldous huxley').
+book_author('lord jim', 'joseph conrad').
+book_author('pale fire', 'vladimir nabokov').
+book_author('heart of darkness', 'joseph conrad').
+book_author('the alexandria quartet', 'lawrence durrell').
---
+++ prolog/musicians.pl
@@ -0,0 +1,17 @@
+musician('frank zappa', guitar).
+musician('steve vai', guitar).
+musician('napoleon murphy brock', saxophone).
+musician('george duke', keyboard).
+musician('chick corea', keyboard).
+musician('john mclaughlin', guitar).
+musician('dave weckl', drums).
+musician('christian mcbride', bass).
+musician('gary willis', bass).
+
+musician_genre('frank zappa', rock).
+musician_genre('steve vai', rock).
+musician_genre('george duke', jazz).
+musician_genre('chick corea', fusion).
+musician_genre('john mclaughlin', fusion).
+musician_genre('dave weckl', fusion).
+musician_genre('gary willis', fusion).
