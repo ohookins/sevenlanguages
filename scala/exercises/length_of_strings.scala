9cb140dc20315dbb474bd27680c61af07d275df3
Oliver Hookins <ohookins@gmail.com>
2013-07-20 11:07:20 +0200

    Calculate total length of all strings in a list in Scala.

---
+++ scala/exercises/length_of_strings.scala
@@ -0,0 +1,5 @@
+val mylist = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.".split(" ").toList
+
+var totalLength = (0 /: mylist) {(sum, element) => sum + element.length}
+
+println("Total length of strings is " + totalLength)
